7a122570d28f08fb1435698c1b60aa0be0027a55
Oleksandr Solovianchyk <49440352+Aladaner@users.noreply.github.com>
2025-07-24 13:32:10 +0300

    Adding data from branch.

--- src/components/Supers.tsx
+++ src/components/Supers.tsx
@@ -1,13 +1,37 @@
-import data from './../helpers/datasheet.json';
 import { cleanSuperData } from '../helpers/cleanData';
 import type { SuperData } from '../types/types';
 import AbilityList from './abilityList/AbilityList';
+import { useEffect, useState } from 'react';
 // import SkillList from './SkillList';
 // import DrawbackList from './DrawbackList';
 
-const superList = cleanSuperData(data as SuperData[]);
+const DATA_URL =
+	'https://raw.githubusercontent.com/Aladaner/dnd/refs/heads/data/src/helpers/datasheet.json';
 
 const Supers = () => {
+	const [superList, setSuperList] = useState<SuperData[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		fetch(DATA_URL)
+			.then((res) => {
+				if (!res.ok) throw new Error(`Ошибка загрузки: ${res.statusText}`);
+				return res.json();
+			})
+			.then((data: SuperData[]) => {
+				const cleaned = cleanSuperData(data);
+				setSuperList(cleaned);
+				setLoading(false);
+			})
+			.catch((err) => {
+				setError(err.message);
+				setLoading(false);
+			});
+	}, []);
+
+	if (loading) return <p>Loading</p>;
+	if (error) return <p>Error: {error}</p>;
 	return (
 		<div>
 			{superList.map((item, index) => (
